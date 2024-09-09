303efa619987e14c813e0db1e271e4e029d8cf0a
pixiee534 <pixiee345@gmail.com>
2024-09-09 18:44:53 +0530

    Introduction To Objects

---
+++ math.js
@@ -0,0 +1,8 @@
+var value = Math.sqrt(30);
+console.log("Demo1 Test Value : " + value + "<br>");
+var value = Math.acos(-1);
+console.log("Demo2 Test Value : " + value + "<br>");
+var value = Math.asin(1);
+console.log("Demo3 Test Value : " + value + "<br>");
+var value = Math.atan(.5);
+console.log("Demo4 Test Value : " + value + "<br>");
---
+++ object.js
@@ -0,0 +1,5 @@
+var person= {
+name:"Akshyani",
+age:"13",
+
+}; console.log(person.name,person.age);
