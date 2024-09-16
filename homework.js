a62bb1fafaf9ef05d313a2eacf57b307ba3bde09
pixiee534 <pixiee345@gmail.com>
2024-09-16 11:55:11 +0530

    homework

---
+++ homework.js
@@ -0,0 +1,16 @@
+var sum = 0;
+[i=0, i<arguments.length, i++]
+sum += arguments[i];
+return sum;
+document.getElementById("demo").innerHTML = sumAll(1, 123,
+    500, 115, 44, 88);
+    var value = Math.sin(45); 
+    console.log=value
+    var value = Math.cos (45); 
+    console.log=value
+    
+
+    
+
+
+    
